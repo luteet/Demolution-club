127b9b81dc22095effd30a78a2069d11054b7ad8
Roman Lukjanets <64837849+luteet@users.noreply.github.com>
2021-11-29 09:56:15 +0200

    v1.0

--- js/main.js
+++ js/main.js
@@ -74,6 +74,40 @@ function loaded() {
   })
 
 
+  var lazyVideos = [].slice.call(document.querySelectorAll("video.lazy"));
+  
+    if ("IntersectionObserver" in window) {
+      var lazyVideoObserver = new IntersectionObserver(function(entries, observer) {
+        entries.forEach(function(video) {
+          if (video.isIntersecting) {
+            for (var source in video.target.children) {
+              var videoSource = video.target.children[source];
+              if (typeof videoSource.tagName === "string" && videoSource.tagName === "SOURCE") {
+                videoSource.src = videoSource.dataset.src;
+                
+              }
+            }
+  
+            video.target.load();
+            video.target.classList.remove("lazy");
+            var obj = document.getElementById('example');
+
+            video.target.addEventListener('loadeddata', function() {
+
+                video.target.classList.add("_visible");
+
+            });
+            lazyVideoObserver.unobserve(video.target);
+          }
+        });
+      });
+  
+      lazyVideos.forEach(function(lazyVideo) {
+        lazyVideoObserver.observe(lazyVideo);
+      });
+    }
+
+
   // =-=-=-=-=-=-=-= СЛАЙДЕРЫ { =-=-=-=-=-=-=-=
 
 /*   const tariffSlider = new Swiper('.tariffs__block', {
